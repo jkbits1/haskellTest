7d4e37ad50d53ae122516e81c1c400c2124641d5
jkbits1 <jkbits1@users.noreply.github.com>
2016-01-24 18:18:39 +0000

    refactoring of functions

--- permCircles.hs
+++ permCircles.hs
@@ -1,6 +1,7 @@
 
 import Data.List
 
+-- test data that is easier to visualise and debug
 --inner =     [1,2,3]
 --second =    [4,5,6]
 --three =     [7,8,9]
@@ -13,63 +14,31 @@ answers = [12, 8, 12, 10, 10, 12, 10, 8]
 
 listLoopItem list chunk = (drop chunk list) ++ (take chunk list)
 
-createListLoops :: [[Int]] -> [Int] -> Int -> [[Int]]
-createListLoops lists seed 0 = lists ++ [seed]
-createListLoops lists seed count =
-    createListLoops
-        (lists ++ [listLoopItem seed count]) seed (count-1)
+listLoops :: [[Int]] -> [Int] -> Int -> [[Int]]
+listLoops lists seed 0 = lists ++ [seed]
+listLoops lists seed count = listLoops (lists ++ [listLoopItem seed count]) seed (count-1)
 
 wheelPerms :: [Int] -> [[Int]]
-wheelPerms xs = createListLoops [] xs $ (length xs) - 1
+wheelPerms xs = listLoops [] xs $ (length xs) - 1
 
 secPerms :: [[Int]]
 secPerms = wheelPerms second
--- [ [4,5,6], [5,4,6] ... ]
+-- [ [4,5,6], [5,6,4] ... ]
 
 thrPerms :: [[Int]]
 thrPerms = wheelPerms three
-
-attachLists :: [Int] -> [Int] -> [[Int]]
-attachLists inner second = inner : second : []
--- params - [1,2,3] [4,5,6]
--- result - [ [1,2,3], [4,5,6] ]
-
-attachInnerList :: [Int] -> [[Int]]
-attachInnerList = attachLists inner
--- param - [4,5,6]
--- result - [ [1,2,3], [4,5,6] ]
+-- [ [7,8,9], [8,9,7] ... ]
 
 twoListPerms :: [[[Int]]]
-twoListPerms = map attachInnerList secPerms
+twoListPerms = map (\sec -> inner : sec : []) secPerms
+--twoListPerms = map attachInnerList secPerms
 -- result - [ [[1,2,3], [4,5,6]],
 --            [[1,2,3], [5,6,4]], ...]
-
--- given two wheels perms and actual answer, create list of diffs
--- create perms of last wheel, then iterate through diffs for a match
 
 --addTuple :: (Int, Int) -> Int
 --addTuple (a, b) = a + b
 minusTuple :: (Int, Int) -> Int
 minusTuple  (a, b) = b - a
-
-sumTwoLists :: [[Int]] -> [Int]
-sumTwoLists lists =
-    let list1   = head lists
-        list2   = head $ drop 1 lists
-        tups    = zip list1 list2
-    in
-        map (\(x, y) -> x + y) tups
-
-summedTwoLists :: [[Int]]
-summedTwoLists = map sumTwoLists twoListPerms
-
-createCandidate list =
-    let tuples = zip list answers
-    in
-        map (\t -> minusTuple t) tuples
-
-candidates = map createCandidate summedTwoLists
-
 
 appendTwoListPerms :: [Int] -> [[[Int]]]
 appendTwoListPerms thr = map (\xs ->  xs ++ [thr]) twoListPerms
@@ -77,17 +46,11 @@ appendTwoListPerms thr = map (\xs ->  xs ++ [thr]) twoListPerms
 -- xs = [[Int]]
 --[ [[1,2,3],[4,5,6],[7,8,9]], [[1,2,3],[5,4,6],[7,8,9]] ...]
 
-
--- meets answer level, but overloads memory.
--- fixed by using loop instead of permutations
-createThreeListPerms :: [[[Int]]]
-createThreeListPerms = concat $ map appendTwoListPerms thrPerms
+threeListPerms :: [[[Int]]]
+threeListPerms = concat $ map appendTwoListPerms thrPerms
 -- result -
---[
---[ [[1,2,3],[4,5,6],[7,8,9]],  [[1,2,3],[5,4,6],[7,8,9]] ... ],
---[ [[1,2,3],[4,5,6],[8,7,9]],  [[1,2,3],[5,4,6],[8,7,9]]  ...]
--- ...
---]
+-- [ [[1,2,3],[4,5,6],[7,8,9]],  [[1,2,3],[5,4,6],[7,8,9]] ...,
+--   [[1,2,3],[4,5,6],[8,7,9]],  [[1,2,3],[5,4,6],[8,7,9]] ... ]
 
 sumTriple :: (Int, Int, Int) -> Int
 sumTriple (a, b, c) = a + b + c
@@ -100,27 +63,11 @@ tuplesFromLists lists =
     in
         zip3 list1 list2 list3
 
-sumLists :: [[Int]] -> [Int]
-sumLists lists = map sumTriple $ tuplesFromLists lists
-
-answersList :: [[Int]]
-answersList = map sumLists createThreeListPerms
-
-answersPermsLoop :: [Int] -> [[Int]]
-answersPermsLoop xs = wheelPerms xs
-
-answersPerms :: [[[Int]]]
-answersPerms = map answersPermsLoop answersList
-
-findSpecificAnswer :: [[Int]]
-findSpecificAnswer =
-    filter (\xs -> xs == answers) $ concat $ answersPerms
-
 sumPlusLists :: [[Int]] -> [([Int], [[Int]])]
 sumPlusLists lists = [(map sumTriple $ tuplesFromLists lists, lists)]
 
 answersPlusList :: [([Int], [[Int]])]
-answersPlusList = concat $ map sumPlusLists createThreeListPerms
+answersPlusList = concat $ map sumPlusLists threeListPerms
 
 answersPermsLoop2 :: ([Int], t) -> ([[Int]], t)
 answersPermsLoop2 (ans, lists) = (wheelPerms ans, lists)
@@ -128,7 +75,7 @@ answersPermsLoop2 (ans, lists) = (wheelPerms ans, lists)
 answersPermsPlusList :: [([[Int]], [[Int]])]
 answersPermsPlusList = map answersPermsLoop2 answersPlusList
 
--- this finds the solution
+-- finds solution
 findSpecificAnswerPlusList :: [([[Int]], [[Int]])]
 findSpecificAnswerPlusList =
     filter (\(ans, lists) -> elem answers ans) answersPermsPlusList
@@ -137,6 +84,73 @@ findSpecificAnswerPlusList =
 displaySpecificAnswers =
     snd $ head findSpecificAnswerPlusList
 
+
+
+--
+--PREPARATORY WORK
+--
+
+-- correct functions, refactored out
+
+--attachLists :: [Int] -> [Int] -> [[Int]]
+--attachLists inner second = inner : second : []
+-- params - [1,2,3] [4,5,6]
+-- result - [ [1,2,3], [4,5,6] ]
+
+--attachInnerList :: [Int] -> [[Int]]
+--attachInnerList = attachLists inner
+-- param - [4,5,6]
+-- result - [ [1,2,3], [4,5,6] ]
+
+
+
+
+-- these functions confirm whether the solution can be found, but do not show which items
+-- create the solution
+
+--sumLists :: [[Int]] -> [Int]
+--sumLists lists = map sumTriple $ tuplesFromLists lists
+--
+--answersList :: [[Int]]
+--answersList = map sumLists createThreeListPerms
+--
+--answersPermsLoop :: [Int] -> [[Int]]
+--answersPermsLoop xs = wheelPerms xs
+--
+--answersPerms :: [[[Int]]]
+--answersPerms = map answersPermsLoop answersList
+--
+--findSpecificAnswer :: [[Int]]
+--findSpecificAnswer =
+--    filter (\xs -> xs == answers) $ concat $ answersPerms
+
+
+
+-- given two wheels perms and actual answer, create list of diffs
+-- create perms of last wheel, then iterate through diffs for a match
+
+--sumTwoLists :: [[Int]] -> [Int]
+--sumTwoLists lists =
+--    let list1   = head lists
+--        list2   = head $ drop 1 lists
+--        tups    = zip list1 list2
+--    in
+--        map (\(x, y) -> x + y) tups
+
+--summedTwoLists :: [[Int]]
+--summedTwoLists = map sumTwoLists twoListPerms
+
+--createCandidate list =
+--    let tuples = zip list answers
+--    in
+--        map (\t -> minusTuple t) tuples
+--
+--candidates = map createCandidate summedTwoLists
+
+
+
+
+-- initial practise with strings, as easier to visualise nested lists
 
 inStr = "abc"
 secStr = "def"
