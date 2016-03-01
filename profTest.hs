1a9d8467eb1060e62dbb81a56af02f39717629bd
jkbits1 <jkbits1@users.noreply.github.com>
2016-03-01 23:50:29 +0000

    foldl' version

--- profTest.hs
+++ profTest.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 import System.Environment
 import Text.Printf
 
@@ -5,7 +7,8 @@ main :: IO ()
 main = do
         [d] <- ((map read) `fmap`) getArgs
 --        printf "%f\n" (mean [1..d])
-        printf "%f\n" (meanFoldl [1..d])
+--        printf "%f\n" (meanFoldl [1..d])
+        printf "%f\n" (meanFoldl' [1..d])
 
 mean :: [Double] -> Double
 mean xs = {-# SCC "mean" #-} sum xs / fromIntegral (length xs)
@@ -14,4 +17,10 @@ meanFoldl :: [Double] -> Double
 meanFoldl xs = s / fromIntegral n
   where
     (n, s)      = foldl k (0, 0) xs
+    k (n, s) x  = (n+1, s+x)
+
+meanFoldl' :: [Double] -> Double
+meanFoldl' xs = s / fromIntegral n
+  where
+    (n, s)      = foldl' k (0, 0) xs
     k (n, s) x  = (n+1, s+x)
