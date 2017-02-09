06dcdc2cdedb0a455aa0ee7596f5a15d219d24bc
jkbits1 <jkbits1@users.noreply.github.com>
2017-02-09 19:29:27 +0000

    state monad code and desugaring

---
+++ StateGame.hs
@@ -0,0 +1,102 @@
+module StateGame where
+
+import Control.Monad.State 
+
+type GameValue = Int
+type GameState = (Bool, Int)
+
+playGame :: String -> State GameState GameValue
+playGame [] = do
+  (_, score) <- get
+  return score
+
+playGame (x:xs) = do
+  (on, score) <- get
+  case x of 
+    'a' | on  -> put (on,     score + 1)
+    'c'       -> put (not on, score)
+  playGame xs
+
+startState :: GameState
+startState = (False, 0)
+
+-- runState (playGame "") startState
+-- runState (playGame "c") startState
+
+-- runState (playGame "ca") startState
+-- runState (playGame "cac") startState
+
+playGameMini (x:[]) = do
+  (on, score) <- get
+  case x of 
+    'c'       -> put (not on, score)
+
+  -- finish part
+  (_, score) <- get
+  return score :: State GameState GameValue
+    
+playGameMini' (x:[]) = do
+  (on, score) <- get
+  case x of 
+    'c'       -> put (not on, score)
+
+  -- finish part
+  pgmBit
+
+pgmBit =
+  get >>= 
+    (\(_, score) -> 
+      return score :: State GameState GameValue)
+
+playGameMini'' (x:[]) =
+  get >>=
+  (\(on, score) ->
+    do
+      case x of 
+        'c'       -> 
+          put (not on, score)
+
+          -- finish part
+      pgmBit
+  )
+
+playGameMini''' (x:[]) =
+  get >>=
+  (\(on, score) ->
+    do
+      case x of 
+        'c'       -> 
+          put (not on, score)
+
+          -- finish part
+      get >>= 
+        (\(_, score) -> 
+          return score :: State GameState GameValue)
+  )
+
+playGameMini'''' (x:[]) =
+  get >>=
+    (\(on, score) ->
+      -- do
+      (case x of 
+        'c'       -> 
+          put (not on, score)) >>= (\_ -> 
+
+        -- finish part
+        get >>= 
+          (\(_, score) -> 
+            return score :: State GameState GameValue)
+            )
+    )
+
+playGameMini''''' (x:[]) =
+  get >>=
+  (\(on, score) ->
+      case x of 
+        'c'       -> do
+          put (not on, score)
+
+          -- finish part
+          pgmBit
+  )
+
